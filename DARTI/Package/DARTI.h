2f0a0f017a5f9b2c8cfd79ca7ef2d9aee630d8dc
Pete <pschuette22@gmail.com>
2024-03-31 16:19:53 -0500

    TypeKeys (#1)

---
+++ DARTI/Package/DARTI.h
@@ -0,0 +1,18 @@
+//
+//  DARTI.h
+//  DARTI
+//
+//  Created by Peter Schuette on 3/31/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DARTI.
+FOUNDATION_EXPORT double DARTIVersionNumber;
+
+//! Project version string for DARTI.
+FOUNDATION_EXPORT const unsigned char DARTIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DARTI/PublicHeader.h>
+
+
